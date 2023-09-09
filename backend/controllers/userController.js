ae3c45cba1073260a1ea1bc19cdfa1e9924cde5d
liangtam <tammieliang@hotmail.com>
2023-09-09 14:42:12 -0700

    JWT generation for signup in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'}); // first arg is payload, second is secret, third is some options
+}
 // login user
 const loginUser = async (req, res) => {
     res.json({mssg: 'login user'})
@@ -14,7 +19,11 @@ const signUpUser = async (req, res) => {
     try {
         const user = await User.signUp(email, password);
 
-        res.status(200).json({email, user});
+        // create a token
+        const token = createToken(user._id);
+
+        // passing the token back to frontend (header, payload, secret all encoded and mushed together into a string, ea item separated via dots)
+        res.status(200).json({email, token});
     } catch (error) {
         res.status(400).json({error: error.message})
     }
