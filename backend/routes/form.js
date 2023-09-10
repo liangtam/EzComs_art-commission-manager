4f6f8c2cfb429c08b44df6b95a60788e04176235
liangtam <tammieliang@hotmail.com>
2023-09-10 13:48:48 -0700

    Protected forms API routes (needs authorization)

--- backend/routes/form.js
+++ backend/routes/form.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getForm, postForm, getForms, deleteForm, updateForm} = require('../controllers/formController');
+const requireAuth = require('../middleware/requireAuth');
 
-// GET the toggled on form
+// require authentication for all form routes
+router.use(requireAuth); // this middleware will be fired before any of the other routes
 
 // GET a form
 router.get('/:id', getForm);
