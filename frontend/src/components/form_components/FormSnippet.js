081c4f822c9b9ca72ff6468a04a6fbcf5cbbdfe0
liangtam <tammieliang@hotmail.com>
2024-01-03 00:07:13 -0800

    Forms styling start

--- frontend/src/components/form_components/FormSnippet.js
+++ frontend/src/components/form_components/FormSnippet.js
@@ -12,6 +12,12 @@ const FormSnippet = ({formId, form, handleDelete}) => {
                     <div className={styles.numOfQuestions}>Number of questions: {form.questions && form.questions.length}</div>
                     <div className={styles.activeStatus}> {form.activeStatus && <div>Active</div>}</div>
                 </div>
+                <div className={styles.questionsPreview}>
+                    <p>Questions Preview:</p>
+                    {form.questions.length !== 0 && form.questions.map((q) => {
+                        return <div className={styles.question}>{q.questionLabel}</div>
+                    })}
+                </div>
             </Link>
             <button className={styles.deleteBtn} onClick={(e) => handleDelete(e, formId)}>Delete</button>
         </div>
--- frontend/src/pages/Form/Forms.js
+++ frontend/src/pages/Form/Forms.js
@@ -1,28 +1,27 @@
-import { useContext, useEffect, useState, useReducer } from "react";
-import FormSnippet from "../../components/form_components/FormSnippet";
-import { FormsContext } from "../../context/FormsContext";
-import styles from './Forms.module.css'
-import { formMessageReducer, ACTION } from "../reducers/formMessageReducer";
-import YesNoPopup from "../../components/form_components/YesNoPopup";
-import { useAuthContext } from "../../hooks/useAuthContext";
-
+import { useContext, useEffect, useState, useReducer } from 'react';
+import FormSnippet from '../../components/form_components/FormSnippet';
+import { FormsContext } from '../../context/FormsContext';
+import styles from './Forms.module.css';
+import { formMessageReducer, ACTION } from '../reducers/formMessageReducer';
+import YesNoPopup from '../../components/form_components/YesNoPopup';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Forms = () => {
-    const {forms, setForms} = useContext(FormsContext);
+    const { forms, setForms } = useContext(FormsContext);
     const [selectedID, setSelectedID] = useState('');
     const [openDeletePopup, setOpenDeletePopup] = useState(false);
     const [state, dispatch] = useReducer(formMessageReducer, {});
 
-    const {user} = useAuthContext();
+    const { user } = useAuthContext();
 
     const fetchAllForms = async () => {
         if (!user) {
             return;
         }
-        dispatch({type: ACTION.LOADING});
+        dispatch({ type: ACTION.LOADING });
         const response = await fetch('http://localhost:4000/api/forms/', {
             headers: {
-                'Authorization': `Bearer ${user.token}`
+                Authorization: `Bearer ${user.token}`
             }
         });
 
@@ -31,17 +30,17 @@ const Forms = () => {
         if (response.ok) {
             setForms(json);
             console.log('forms in all forms pg: ', forms);
-            dispatch({type: ACTION.RESET});
+            dispatch({ type: ACTION.RESET });
         } else {
-            dispatch({type: ACTION.ERROR_GET_ALL});
+            dispatch({ type: ACTION.ERROR_GET_ALL });
         }
         setOpenDeletePopup(false);
-    }
+    };
 
     const handleOpenDeletePopup = (e, selectedID) => {
         setOpenDeletePopup(true);
         setSelectedID(selectedID);
-    }
+    };
 
     const handleDelete = async (e) => {
         if (!user) {
@@ -51,46 +50,50 @@ const Forms = () => {
         const response = await fetch('http://localhost:4000/api/forms/' + selectedID, {
             method: 'DELETE',
             headers: {
-                'Authorization': `Bearer ${user.token}`
+                Authorization: `Bearer ${user.token}`
             }
-        })
+        });
 
         if (response.ok) {
             console.log('Form deleted!');
             await fetchAllForms();
-            dispatch({type: ACTION.SUCCESS_DELETE});
+            dispatch({ type: ACTION.SUCCESS_DELETE });
             setTimeout(() => {
-                dispatch({type: ACTION.RESET})
-            }, 3000)
+                dispatch({ type: ACTION.RESET });
+            }, 3000);
         }
         setOpenDeletePopup(false);
-    }
+    };
 
     useEffect(() => {
         if (user) {
             fetchAllForms();
         }
         console.log('fetched');
-    }, [user])
+    }, [user]);
 
     return (
         <div className={styles.formsContainer}>
-            {state.errorMessage && <div className={styles.errorMessage}>{state.errorMessage}</div>}
+            <div className={styles.title}>
+                <h1> Forms </h1>
+            </div>
+            {state.errorMessage && <div className="errorMessage">{state.errorMessage}</div>}
             {state.successMessage && <div className={styles.successMessage}>{state.successMessage}</div>}
             {state.loadingMessage && <div className={styles.loadingMessage}>{state.loadingMessage}</div>}
             <div className={styles.forms}>
-            {openDeletePopup &&
-            <YesNoPopup yesFunction={handleDelete} closePopup={(e) => setOpenDeletePopup(false)}>
-                <h3>Are you sure?</h3>
-                <p>Are you sure you want to delete this form? This action cannot be undone.</p>
-            </YesNoPopup>}
-            {forms && forms.map((form) => {
-                return <FormSnippet formId={form._id} form={form} handleDelete = {handleOpenDeletePopup} key={form._id}/>
-            })}
+                {openDeletePopup && (
+                    <YesNoPopup yesFunction={handleDelete} closePopup={(e) => setOpenDeletePopup(false)}>
+                        <h3>Are you sure?</h3>
+                        <p>Are you sure you want to delete this form? This action cannot be undone.</p>
+                    </YesNoPopup>
+                )}
+                {forms &&
+                    forms.map((form) => {
+                        return <FormSnippet formId={form._id} form={form} handleDelete={handleOpenDeletePopup} key={form._id} />;
+                    })}
+            </div>
         </div>
-        </div>
-
-    )
-}
+    );
+};
 
 export default Forms;
