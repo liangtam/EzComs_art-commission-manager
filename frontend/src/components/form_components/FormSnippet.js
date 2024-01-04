e7f22c8af53b3d2da9fd3a1f41edfab646f49a03
liangtam <tammieliang@hotmail.com>
2024-01-03 19:06:02 -0800

    Forms styling

--- frontend/src/components/form_components/FormSnippet.js
+++ frontend/src/components/form_components/FormSnippet.js
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import trashIcon from '../../public/images/delete_trash.png';
+import editIcon from '../../public/images/edit_icon.png';
 
 import styles from './FormSnippet.module.css';
 
 const FormSnippet = ({ formId, form, handleDelete }) => {
+    const nav = useNavigate();
     return (
         <div className={styles.formSnippet}>
             <Link className={styles.link} to={`/forms/${formId}`}>
@@ -14,19 +16,27 @@ const FormSnippet = ({ formId, form, handleDelete }) => {
                     <div className={styles.numOfQuestions}>Number of questions: {form.questions && form.questions.length}</div>
                 </div>
                 <div className={styles.questionsPreview}>
-                    <p>Questions Preview:</p>
-                    {form.questions.length !== 0 && <div className={styles.question}>{form.questions[0].questionLabel}</div>}
-                    {form.questions[1] !== undefined && <div className={styles.question}>{form.questions[1].questionLabel}</div>}
-
+                    <p>
+                        <b>Preview:</b>
+                    </p>
+                    <ul>
+                        {form.questions.length !== 0 && <li>{form.questions[0].questionLabel}</li>}
+                        {form.questions[1] !== undefined && <li>{form.questions[1].questionLabel}</li>}
+                    </ul>
+                    <p>{form.questions.length >= 3 && 'Read more'}</p>
                 </div>
+            </Link>
+            <div className={styles.bottom}>
                 {form.activeStatus && (
                     <div className={styles.activeStatus}>
                         <b>Active</b>
                     </div>
                 )}
-            </Link>
-            <div className={styles.actions}>
-            <img src={trashIcon} alt="Delete" onClick={(e) => handleDelete(e, formId)}></img>
+                <div className={styles.actions}>
+                    <img src={editIcon} alt="Edit" onClick={(e) => nav(`/forms/${formId}`)}></img>
+
+                    <img src={trashIcon} alt="Delete" onClick={(e) => handleDelete(e, formId)}></img>
+                </div>
             </div>
         </div>
     );
